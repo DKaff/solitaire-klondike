9b4c85f1a7269b56559c4e3b711442964a1121a4
DKaff <dkassay1@gmail.com>
2025-05-29 01:00:17 -0400

    Packaged with Electron Builder

---
+++ electron/main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 900,
+    height: 700,
+    webPreferences: {
+      contextIsolation: false,
+      nodeIntegration: true,
+    },
+  });
+
+  // Load the React build output directly:
+  win.loadFile(path.join(__dirname, '..', 'build', 'index.html'));
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
